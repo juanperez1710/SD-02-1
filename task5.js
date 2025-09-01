0cf9211f75cd5504d029f0dfc709c2d7ec32e3a5
juanperez1710 <junpemarc@gmail.com>
2025-09-01 16:09:05 -0600

    Se realizan las tareas del 1 al 5

--- task5.js
+++ task5.js
@@ -1,5 +1,19 @@
 // Refer to Task 5 in your Instructions to complete this task
-
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Five!");
+const prompt = require("prompt-sync")();
+const numero=+prompt("Ingresa el número de veces que quieres hacer el Fizz-Buzz");
+for (let i = 1; i < numero+1; i++) {
+  if (i % 15 === 0){
+    console.log("Fizz-Buzz")
+  }
+   else if (i % 7=== 0) {
+    console.log("Woof");
+  }
+  else if (i % 5=== 0) {
+    console.log("Buzz");
+  }
+  else if(i % 3 === 0){
+    console.log("Fizz")
+  }
+  else
+  console.log(i);
   };
