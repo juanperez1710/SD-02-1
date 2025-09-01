1adfc264970061952a3c1d76cb1f039f800af6ff
juanperez1710 <junpemarc@gmail.com>
2025-09-01 16:11:24 -0600

    Modificacion en tarea 5

--- task5.js
+++ task5.js
@@ -1,7 +1,7 @@
 // Refer to Task 5 in your Instructions to complete this task
 const prompt = require("prompt-sync")();
 const numero=+prompt("Ingresa el número de veces que quieres hacer el Fizz-Buzz");
-for (let i = 1; i < numero+1; i++) {
+for (let i = 1; i <= numero; i++) {
   if (i % 15 === 0){
     console.log("Fizz-Buzz")
   }
